399b45d895312e4eeadfdd42810488ba0e1a02f3
Brandon <trianab659@gmail.com>
2023-07-13 15:14:24 -0500

    added new fuction to split destination

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -3,33 +3,41 @@ import { View, Text, TouchableOpacity, StyleSheet, Linking } from 'react-native'
 import packeges from '../../components/packages.json';
 
 const Home = () => {
-  const [destinations, setDestinations] = useState<any>([]);
+  const [allDestinations, setAllDestinations] = useState<any>([]);
 
-  const openGoogleMaps = async () => {
-    const origin = `${destinations[0].lat}%2C${destinations[0].lng}`;
-    const baseUrl = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=`;
-    const nextURl = `${origin}&travelmode=driving`;
+  const openGoogleMaps = async (destinations: any) => {
+    const destination = `${destinations[destinations.length - 1].lat}%2C${destinations[destinations.length - 1].lng}`;
+    const baseUrl = `https://www.google.com/maps/dir/?api=1&origin=&destination=${destination}&travelmode=driving`;
     const waypoints = destinations.map((element: any) => {
       return `${element.lat}%2C${element.lng}`;
     });
-    waypoints.shift();
-    waypoints.pop();
-    await Linking.openURL(`${baseUrl}${nextURl}&waypoints=${waypoints.join('%7C')}`);
+    waypoints.pop()
+    await Linking.openURL(`${baseUrl}&waypoints=${waypoints.join('%7C')}`);
   };
 
   useEffect(() => {
-    const newDestinationsArray: any = [];
-    packeges.map((eachPackage) => {
-      newDestinationsArray.push({ lat: eachPackage.lat, lng: eachPackage.lng });
-    });
-    setDestinations(newDestinationsArray);
+    const newDestinationsArray = packeges.reduce((result: any, object, index) => {
+      const chunkIndex = Math.floor(index / 10);
+      if (!result[chunkIndex]) {
+        result[chunkIndex] = []; 
+      }
+      result[chunkIndex].push({lat: object.lat, lng: object.lng}); 
+      
+      return result;
+    }, []);
+
+    setAllDestinations(newDestinationsArray)
   }, []);
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.btn} onPress={openGoogleMaps}>
-        <Text style={styles.text}>Open Google Maps</Text>
-      </TouchableOpacity>
+      {
+        allDestinations.map((destinations: any, index: number) => (
+        <TouchableOpacity style={styles.btn} onPress={() => openGoogleMaps(destinations)} key={index}>
+          <Text style={styles.text}>Open {index+1} Route</Text>
+        </TouchableOpacity>
+        ))
+      }
     </View>
   );
 };
@@ -38,12 +46,14 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 80,
     flexGrow: 1,
   },
   btn: {
     backgroundColor: '#457b9d',
     padding: 16,
     borderRadius: 12,
+    marginBottom: 15,
   },
   text: {
     color: '#fff',
