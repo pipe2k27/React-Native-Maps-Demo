f8eefc327bf8dd4e987729a8b559bf7b7a3aeaaf
Facundo Colombier <fcolombier@teravisiontech.com>
2023-07-18 17:29:44 -0300

    cleanup

--- src/screens/Login/Login.screen.tsx
+++ src/screens/Login/Login.screen.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 import { DipCoreLogo, NriBlueLogo } from '../../assets';
-import { styles } from './Login.styles';
 import { Button, StyledText, StyledTextInput } from '../../components';
-import { useNavigation } from '@react-navigation/native';
+import { styles } from './Login.styles';
 
 export const Login = ({ navigation }: any) => (
   <View style={{ flex: 1 }}>
